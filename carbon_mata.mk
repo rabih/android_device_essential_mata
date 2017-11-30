778905cd786504af867d49caf4c59dd32a4a9cc5
Rabih Salamey <somcom3@gmail.com>
2017-11-29 19:39:16 -0500

    mata: carbonify from LOS tree
    Change-Id: I78ca07cc69c420f4612996563c0a7d63c938a212
    
    mata: move deps to carbon
    
    Change-Id: Ic777f735a501e140870987501e1e313e515decde

--- carbon_mata.mk
+++ carbon_mata.mk
@@ -1,6 +1,7 @@
-# Inherit some common Lineage stuff.
+# Inherit some common Carbon stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common.mk)
+$(call inherit-product, vendor/carbon/config/gsm.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
@@ -59,7 +60,7 @@ endif
 
 # Device identifiers
 PRODUCT_DEVICE := mata
-PRODUCT_NAME := lineage_mata
+PRODUCT_NAME := carbon_mata
 PRODUCT_BRAND := essential
 PRODUCT_MODEL := PH-1
 PRODUCT_MANUFACTURER := Essential Products
